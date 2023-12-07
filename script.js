5bdb3a7f2237871d207f8f195d3ae6d90204374a
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2023-12-06 19:05:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let hours = document.getElementById("hours");
+let mins = document.getElementById("mins");
+let seconds = document.getElementById("seconds");
+
+setInterval(() => {
+	let currentTime = new Date();
+
+	hours.innerHTML = (currentTime.getHours() <10? "0":"") + currentTime.getHours();
+	mins.innerHTML = (currentTime.getMinutes() <10? "0":"") + currentTime.getMinutes();
+	seconds.innerHTML = (currentTime.getSeconds() <10? "0":"") + currentTime.getSeconds();
+
+    if(currentTime.getHours() > 12){
+        hours.innerHTML = (currentTime.getHours() % 12)
+    }
+
+});
